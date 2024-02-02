fdaa8e88e34d72363c9a4aa5b249ba017f5680e3
Antony Gitonga <antony@allenhark.com>
2024-02-02 12:57:42 +0300

    Removed unessecary bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/globals.css'
 import '@/styles/globals-responsive.css'
 
@@ -7,10 +6,10 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-	  const getLayout = Component.getLayout || ((page) => page);
-  return   (
-  <>
-  {getLayout(<Component {...pageProps} />)}
-  </>
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    <>
+      {getLayout(<Component {...pageProps} />)}
+    </>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-	
+
       <Head>
-	    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossrigin/>
-		<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-	    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-	  </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous" />
+      </Head>
       <body>
         <Main />
         <NextScript />
